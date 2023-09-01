ee8e4130650f51ccab860bf84dc965c649d1dba4
hlumelo-web <61595056+hlumelo-web@users.noreply.github.com>
2023-09-01 15:23:26 +0200

    Add files via upload

---
+++ MergeSort.java
@@ -0,0 +1,78 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package mergesort;
+
+/**
+ *
+ * @author HLUMELO
+ */
+import java.util.Arrays;
+
+public class MergeSort {
+    public static void main(String[] args) {
+        int[] arr = {38, 27, 43, 3, 9, 82, 10};
+        System.out.println("Original array: " + Arrays.toString(arr));
+
+        mergeSort(arr);
+
+        System.out.println("Sorted array: " + Arrays.toString(arr));
+    }
+
+    public static void mergeSort(int[] arr) {
+        int n = arr.length;
+        if (n < 2) {
+            return; // Base case: If the array has 0 or 1 elements, it's already sorted.
+        }
+
+        int mid = n / 2;
+        int[] left = new int[mid];
+        int[] right = new int[n - mid];
+
+        // Populate the left and right subarrays
+        for (int i = 0; i < mid; i++) {
+            left[i] = arr[i];
+        }
+        for (int i = mid; i < n; i++) {
+            right[i - mid] = arr[i];
+        }
+
+        // Recursively sort the left and right subarrays
+        mergeSort(left);
+        mergeSort(right);
+
+        // Merge the sorted subarrays
+        merge(arr, left, right);
+    }
+
+    public static void merge(int[] arr, int[] left, int[] right) {
+        int n1 = left.length;
+        int n2 = right.length;
+        int i = 0, j = 0, k = 0;
+
+        // Merge elements from left and right back into arr
+        while (i < n1 && j < n2) {
+            if (left[i] <= right[j]) {
+                arr[k] = left[i];
+                i++;
+            } else {
+                arr[k] = right[j];
+                j++;
+            }
+            k++;
+        }
+
+        // Copy any remaining elements from left and right (if any)
+        while (i < n1) {
+            arr[k] = left[i];
+            i++;
+            k++;
+        }
+        while (j < n2) {
+            arr[k] = right[j];
+            j++;
+            k++;
+        }
+    }
+}
